33abff1e6ef47224d8634d0fe08f08e961cd6f8c
fede suarez <fedesuarez79@gmail.com>
2016-05-21 13:49:43 -0300

    commiteando script desde donbeto

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-git pull origin master
 
-ls
+# if no output from the remote ssh cmd, bail out
+if [ -z "git pull origin master" ]; then
+    echo "No results from remote repo listing (via SSH)"
+    exit
+fi
+
+echo "git responde ok"
 git add script*
 git commit -m "commiteando script desde donbeto"
 git push origin master
