492f03d2c1af7a5fe63d59888289d4f1bbcc843d
fede suarez <fedesuarez79@gmail.com>
2016-05-21 14:00:10 -0300

    commiteando script desde donbeto

--- script.sh
+++ script.sh
@@ -12,9 +12,9 @@ echo "git pull ok"
 
 git add script*
 git commit -m "commiteando script desde donbeto"
-comando=$(git push origin master)
-if [ -z "$comando" ]; then
-        if [ -z "$comando" ]; then
+comando2=$(git push origin master)
+if [ -z "$comando2" ]; then
+        if [ -z "$comando2" ]; then
                 echo "git push no responde"
                 exit
         fi
